5edfa4f03aab20bebd74546be09a03f16433abd5
Bayazid B. Halim <bayazid.bustami.halim@gmail.com>
2023-07-28 10:26:04 +0700

    add comments for documentation

--- flake.nix
+++ flake.nix
@@ -1,31 +1,31 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # use nixpkgs-unstable as main nixpkgs source
     home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager"; # home-manager unstable url
+      inputs.nixpkgs.follows = "nixpkgs"; # inherit nixpkgs-unstable as main nixpkgs source
     };
-    flatpaks.url = "github:GermanBread/declarative-flatpak/stable";
+    flatpaks.url = "github:GermanBread/declarative-flatpak/stable"; # declarative-flatpak, still WIP
     # Add other inputs if needed
   };
 
   outputs = { self, nixpkgs, home-manager, flatpaks }:
   {
     # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake
     homeConfigurations = {
+    # declare username@hostname specific configuration
       "fenglengshun@ostree-pc" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         modules = [
-          ./pc/home.nix
-          flatpaks.homeManagerModules.default
+          ./pc/home.nix # device specific home.nix
+          flatpaks.homeManagerModules.default # import declarative-flatpak module
         ];
       };
       "fenglengshun@neon-laptop" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         modules = [
-          ./laptop/home.nix
-          flatpaks.homeManagerModules.default
+          ./laptop/home.nix # device specific home.nix
+          flatpaks.homeManagerModules.default # import declarative-flatpak module
         ];
       };
     };
