3c1ddd83f235438ef25aada74b330110c3b6ff12
Bayazid B. Halim <bayazid.bustami.halim@gmail.com>
2023-08-06 09:33:54 +0700

    checking flakes issue

--- flake.nix
+++ flake.nix
@@ -5,23 +5,9 @@
       url = "github:nix-community/home-manager"; # home-manager unstable url
       inputs.nixpkgs.follows = "nixpkgs"; # inherit nixpkgs-unstable as main nixpkgs source
     };
-    flatpaks.url = "github:GermanBread/declarative-flatpak/dev"; # declarative-flatpak, still WIP
-    nixgl.url = "github:guibou/nixGL"; # nixGL for running Wine
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable"; # https://github.com/chaotic-cx/nyx#how-to-use-it}
-    # Add other inputs if needed
-  };
+    };
 
-  outputs = { self, nixpkgs, home-manager, flatpaks, nixgl, chaotic }:
-  let
-
-    # System types to support.  [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-    supportedSystems = [ "x86_64-linux" ];
-    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    # Nixpkgs instantiated for supported system types.
-    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
-  in
+  outputs = { self, nixpkgs, home-manager }:
   {
     # Standalone home-manager configuration entrypoint
     homeConfigurations = {
@@ -37,15 +23,10 @@
           ./default/files.nix # file creation for all devices
           ./pc/by-device.nix # device specific configs
           ./pc/flatpak.nix # separate list for flatpak
-          ./pc/chaotic.nix # separate list for chaotic.nix packages
-          ./pc/nixgl.nix # separate list for nixgl.nix package
-          flatpaks.homeManagerModules.default # declarative-flatpak HM module
-          chaotic.homeManagerModules.default # chaotic nyx HM module
         ];
       };
       "fenglengshun@neon-laptop" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = { inherit nixgl; }; # so that home-manager can correctly read chaotic.packages
         modules = [
           ./home.nix # default home.nix
           ./default/shell.nix # shell config for all devices
@@ -53,19 +34,8 @@
           ./default/alias.nix # aliases for all devices
           ./default/files.nix # file creation for all devices
           ./laptop/by-device.nix # device specific configs
-          ./laptop/flatpak.nix # separate list for flatpak
-          ./laptop/chaotic.nix # separate list for chaotic.nix package
-          ./laptop/nixgl.nix # separate list for nixgl.nix package
-          flatpaks.homeManagerModules.default # import declarative-flatpak module
-          chaotic.homeManagerModules.default # chaotic nyx HM module
         ];
       };
-      # "root" = home-manager.lib.homeManagerConfiguration {
-      #   pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-      #   modules = [
-      #     ./root/home.nix # default home.nix
-      #   ];
-      # };
     };
   };
 }
