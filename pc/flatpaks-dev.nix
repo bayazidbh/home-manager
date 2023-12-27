4e7d254fafbc498c400107e3c8ad861ca6e747f2
bayazidbh <bayazid.bustami.halim@gmail.com>
2023-12-27 17:40:21 +0700

    update flatpak & configs

--- pc/flatpaks-dev.nix
+++ pc/flatpaks-dev.nix
@@ -56,10 +56,10 @@ services.flatpak.flatpak = {
      "flathub:app/com.usebottles.bottles/x86_64/stable"
      "flathub:app/org.winehq.Wine/x86_64/stable-23.08"
 
-     "flathub:app/com.valvesoftware.Steam/x86_64/stable"
+     # "flathub:app/com.valvesoftware.Steam/x86_64/stable"
      # "flathub:runtime/com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable"
-     "flathub:runtime/com.valvesoftware.Steam.Utility.steamtinkerlaunch/x86_64/stable"
-     "flathub:runtime/com.valvesoftware.Steam.Utility.thcrap_steam_proton_wrapper/x86_64/stable"
+     # "flathub:runtime/com.valvesoftware.Steam.Utility.steamtinkerlaunch/x86_64/stable"
+     # "flathub:runtime/com.valvesoftware.Steam.Utility.thcrap_steam_proton_wrapper/x86_64/stable"
      "flathub:runtime/org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/23.08"
      "flathub:runtime/com.valvesoftware.Steam.Utility.gamescope/x86_64/stable"
      "flathub:runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/23.08"
--- pc/flatpaks.nix
+++ pc/flatpaks.nix
@@ -55,10 +55,10 @@
      "flathub:com.usebottles.bottles/x86_64/stable"
      "flathub:org.winehq.Wine/x86_64/stable-23.08"
 
-     "flathub:com.valvesoftware.Steam/x86_64/stable"
+     # "flathub:com.valvesoftware.Steam/x86_64/stable"
      # "flathub:com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable"
-     "flathub:com.valvesoftware.Steam.Utility.steamtinkerlaunch/x86_64/stable"
-     "flathub:com.valvesoftware.Steam.Utility.thcrap_steam_proton_wrapper/x86_64/stable"
+     # "flathub:com.valvesoftware.Steam.Utility.steamtinkerlaunch/x86_64/stable"
+     # "flathub:com.valvesoftware.Steam.Utility.thcrap_steam_proton_wrapper/x86_64/stable"
      "flathub:org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/23.08"
      "flathub:com.valvesoftware.Steam.Utility.gamescope/x86_64/stable"
      "flathub:org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/23.08"
